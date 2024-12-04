de52f75a71a97cb0322d408446f2b6a5d1c281f7
Riya <riya01022001@gmail.com>
2024-12-04 10:45:03 +0530

    Day2

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-{/* <div id="parent">
-        <div id="child">
-            <h1>I am an h1 Tag</h1>
-            <h2>I am an h1 Tag</h2>
-        </div>
-        <div id="child2">
-            <h1>I am an h1 Tag</h1>
-            <h2>I am an h1 Tag</h2>
-        </div>
-</div> */}
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 
-//ReactElement(object) = HTML(Browser Understands)
 
 const parent = React.createElement("div",{id:"parent"},
-[React.createElement("div",{id:"child"},React.createElement("h1",{},"I am an h1 Tag"),React.createElement("h2",{},"I am an h2 Tag"))] ,
-[   React.createElement("div",{id:"child2"},React.createElement("h1",{},"I am an Child2 h1 Tag"),React.createElement("h2",{},"I am an Child2 h2 Tag"))])
+[React.createElement("div",{id:"child"},React.createElement("h1",{},"This is a Namaste React"),React.createElement("h2",{},"I am an h2 Tag"))] ,
+[   React.createElement("div",{id:"child2"},React.createElement("h1",{},"By Riya"),React.createElement("h2",{},"I am an Child2 h2 Tag"))])
 
 console.log(parent);//object
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste-React
+
+# Parcel 
+- Dev Build
+- Local server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - write in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistant Hashing
+- Code splitting
+- Diffrential Bundling - support older browsers
+- Diagnostics
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Diffrent dev and prod bundles 
