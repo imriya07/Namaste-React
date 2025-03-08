2320e3bbb2fd05a9191dd2680983d20151267460
Riya <riya01022001@gmail.com>
2025-03-08 10:16:27 +0530

    namaste-react-codebase

---
+++ App.js
@@ -0,0 +1,22 @@
+// const heading = React.createElement("h1", {id:"heading"}, "Hello world from React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// nested Element
+// React.createElement(object) = HTML((Browser understands))
+
+// const div = React.createElement("div", {id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"I am H1 Tag!")));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(div)
+
+const div = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 Tag"),
+    React.createElement("h2", {}, "I am an h2 Tag"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(div);
